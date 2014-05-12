74211e7429177d902f2bc1752a0e7b4c28228a53
Stian Johnsen <stianjohns@gmail.com>
2014-05-12 22:23:19 +0200

    Change to setup.sh

--- setup.sh
+++ setup.sh
@@ -9,3 +9,5 @@ ln -sf $HOME/config/bashrc.d/ $HOME/.bashrc.d
 ln -sf $HOME/config/tmux/tmux.conf $HOME/.tmux.conf
 
 git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+ln -sf $HOME/config/vim/vimrc $HOME/.vimrc
+vim +PluginInstall +qall

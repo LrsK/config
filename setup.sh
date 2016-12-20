fdf6c25bc1386fb8a9a3a2a721e60324730a3109
LrsK <larskulseng@gmail.com>
2016-12-20 12:37:13 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+GOVERSION=1.7.4
+
 sudo_command=$(which sudo)
 
 YUM=`which yum 2>/dev/null`
@@ -17,27 +19,44 @@ if [ -z "$YUM" -a -z "$APT" ]; then
 fi
 
 if [ ! -z "$YUM" ]; then
-	$sudo_command $YUM install -y zsh wget
+	$sudo_command $YUM install -y zsh wget git
 
 fi
 
 if [ ! -z "$APT" ]; then
-	$sudo_command $APT install -y zsh wget
+	$sudo_command $APT install -y zsh wget git
 fi
 
 # Install oh-my-zsh
 sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+
+# Install pathogen
+mkdir -p ~/.vim/autoload ~/.vim/bundle && \
+git clone https://github.com/tpope/vim-pathogen ~/.vim/autoload/pathogen.vim
+
+# Install Go
+GOFILE=go${GOVERSION}.linux-amd64.tar.gz
+wget https://storage.googleapis.com/golang/${GOFILE}
+$sudo_command rm -rf /usr/local/go
+$sudo_command tar -C /usr/local -xzf ${GOFILE}
+rm go${GOVERSION}.linux-amd64.tar.gz
+
+# Install vim-go-ide
+git clone git@github.com:farazdagi/vim-go-ide.git ~/.vim_go_runtime
+sh ~/.vim_go_runtime/bin/install
 
 # Add stuff to .zshrc
 if ! grep -qe "# Added by \$HOME/config/setup.sh" "$HOME/.zshrc"; then    
 	cat zshrc.sh >> $HOME/.zshrc
 fi
 
-# zsh setting
+# bash/zsh settings
 ln -sf $HOME/config/bashrc.d/ $HOME/.bashrc.d
 # tmux settings
 ln -sf $HOME/config/tmux/tmux.conf $HOME/.tmux.conf
 
 git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+# vim settings
 ln -sf $HOME/config/vim/vimrc $HOME/.vimrc
+# Install pliugins through vundle
 vim +PluginInstall +qall
